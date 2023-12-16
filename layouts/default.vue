144fd41afa936aebdb7a294a3a57fcc696c8ada1
Amir Abroun <abroon234@gmail.com>
2023-12-16 15:49:39 +0330

    improv: update default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,29 @@
 <template>
   <client-only>
-    <VContainer class="pa-2 elevation-10 rounded-b-lg ma-0 pl-5 navbar" fluid>
+    <VContainer class="py-1 elevation-4 rounded-b-lg navbar" fluid>
       <VRow justify="start" align="center">
         <VCol cols="auto">
-          <VAvatar v-if="authUser">
+          <NuxtLink to="/" class="logo-link">
+            <img src="@/assets/images/logo-e-blog.png" class="logo-img" />
+          </NuxtLink>
+        </VCol>
+        |
+        <VCol cols="auto" class="ml-2" v-if="authUser">
+          <VAvatar>
             <NuxtLink v-if="authUser.avatar != null">
-              <img
-                :src="authUser.avatar"
-                width="40"
-                height="40"
-                class="rounded avatar-img"
-                @error="handleImageError"
-              />
+              <img :src="authUser.avatar" width="40" height="40" class="avatar-img" @error="handleImageError" />
             </NuxtLink>
             <NuxtLink v-else>
-              <img
-                src="@/assets/images/avatar.png"
-                width="40"
-                height="40"
-                class="rounded avatar-img"
-              />
+              <img src="@/assets/images/avatar.png" width="40" height="40" class="rounded avatar-img" />
             </NuxtLink>
           </VAvatar>
-          <VMenu activator="parent" v-if="authUser">
+          <span class="ml-3">{{ authUser.full_name }} </span>
+          <VMenu activator="parent">
             <VList>
               <VListItem :to="`/users/${authUser?.uuid}/resume`">
                 <VListItemTitle class="text-black">
-                  <VIcon class="mr-2"> mdi-account</VIcon
-                  >Profile</VListItemTitle
-                >
+                  <VIcon class="mr-2"> mdi-account</VIcon>Profile
+                </VListItemTitle>
               </VListItem>
               <VListItem @click="logout">
                 <VListItemTitle class="text-red">
@@ -39,38 +34,20 @@
             </VList>
           </VMenu>
         </VCol>
-
-        <VCol cols="auto" class="px-0">
-          <NuxtLink to="/" class="logo-link pb-2">
-            <img src="@/assets/images/logo-e-blog-1.png" class="logo-img" />
-          </NuxtLink>
-        </VCol>
         <VCol cols="auto" v-if="!authUser">
-          <NuxtLink
-            to="/login"
-            class="signIn-link navbar-link py-3 px-4 rounded"
-            >Sign In</NuxtLink
-          >
-        </VCol>
-        <VCol cols="auto" v-if="!authUser">
-          <NuxtLink
-            to="/register"
-            class="signUp-link navbar-link py-3 px-4 rounded"
-            >Sign Up</NuxtLink
-          >
-        </VCol>
-        <VCol cols="auto" v-if="authUser && authUser.is_admin == true">
-          <NuxtLink to="/users" class="text-decoration-none">Users</NuxtLink>
+          <NuxtLink to="/login" class="navbar-link">Sign In</NuxtLink>
+          <NuxtLink to="/register" class="signUp-link navbar-link ml-4 py-2 px-4 rounded-lg">Sign Up</NuxtLink>
         </VCol>
       </VRow>
     </VContainer>
-    <VContainer fluid class="mt-15">
+    <VContainer fluid class="mt-12">
       <slot />
     </VContainer>
   </client-only>
 </template>
 <script lang="ts" setup>
 import { Status, store } from "~/store";
+import { auth } from "~/store/auth";
 import { AuthActionTypes } from "~/store/auth/action-types";
 const router = useRouter();
 const authUser = computed(() => store.state.auth?.authUser);
@@ -141,16 +118,10 @@ a {
 }
 
 .navbar-link {
-  font-weight: 700;
-  color: #00416a;
-  transition: 0.2s;
-}
-
-.navbar-link:hover {
-  background-color: rgba($color: #fff, $alpha: 0.4);
+  color: #00416a9f;
 }
 
 .signUp-link {
-  border: 2px solid;
+  border: 1px solid;
 }
 </style>
