344f601144199df02f141a5daa8b0f2c0821e470
Reza <i.am.reza.programmer@gmail.com>
2024-01-04 08:33:21 +0330

    feat(user page): add toggle follow

--- pages/users/[uuid]/index.vue
+++ pages/users/[uuid]/index.vue
@@ -3,7 +3,11 @@
     <VRow justify="center">
       <VCol cols="12" md="7">
         <VCard class="pa-2 mb-3" v-for="post in posts" :key="post.id">
-          <VImg :src="post.media?.find(() => true)?.original_url" max-height="400" class="rounded-lg"></VImg>
+          <VImg
+            :src="post.media?.find(() => true)?.original_url"
+            max-height="400"
+            class="rounded-lg"
+          ></VImg>
           <VCardTitle class="text-h5">{{ post.title }}</VCardTitle>
           <VCardText>{{ post.body }}</VCardText>
           <VCardSubtitle>
@@ -17,8 +21,16 @@
           <VRow>
             <VCol cols="auto">
               <v-avatar size="50" class="rounded avatar">
-                <img :src="user?.avatar" v-if="user?.avatar" class="rounded w-100" />
-                <img src="@/assets/images/avatar.png" v-else class="rounded w-100" />
+                <img
+                  :src="user?.avatar"
+                  v-if="user?.avatar"
+                  class="rounded w-100"
+                />
+                <img
+                  src="@/assets/images/avatar.png"
+                  v-else
+                  class="rounded w-100"
+                />
               </v-avatar>
             </VCol>
             <VCol cols="8">
@@ -28,7 +40,12 @@
           </VRow>
           <VRow justify="center" align="center" class="px-2 py-3">
             <VCol cols="12" v-if="authUser" class="pa-0">
-              <VBtn class="w-100" :color="followBtnColor" @click="sendFollowing(followBtnText)" ref="followBtn">
+              <VBtn
+                class="w-100"
+                :color="followBtnColor"
+                @click="sendFollowing(followBtnText)"
+                ref="followBtn"
+              >
                 {{ followBtnText }}
               </VBtn>
             </VCol>
@@ -36,19 +53,27 @@
         </VCard>
       </VCol>
     </VRow>
-    <VBtn v-if="authUser" fab dark color="secondary" :to="'/posts/create'" class="fixed-bottom-right btn">
+    <VBtn
+      v-if="authUser"
+      fab
+      dark
+      color="secondary"
+      :to="'/posts/create'"
+      class="fixed-bottom-right btn"
+    >
       <VIcon>mdi-plus</VIcon>
     </VBtn>
   </VContainer>
 </template>
 <script lang="ts" setup>
+import { setTimeout } from "timers";
 import { store } from "~/store";
 import { UserActionTypes } from "~/store/user/actions";
 import { UsersActionTypes } from "~/store/users/actions";
 const route = useRoute();
 const followBtn = ref();
-let followBtnText: string = "Follow";
-let followBtnColor: string = "success";
+let followBtnText = ref("Follow");
+let followBtnColor = ref("success");
 
 async function sendFollowing(text: string) {
   await store.dispatch(
@@ -59,6 +84,16 @@ async function sendFollowing(text: string) {
     `user/${UserActionTypes.fetchUserPosts}`,
     route.params.uuid
   );
+  if (
+    store.state.user?.user.auth_followed_at &&
+    store.state.user?.user.follow_accepted_at
+  ) {
+    followBtnText.value = "Unfollow";
+    followBtnColor.value = "red";
+  } else {
+    followBtnText.value = "Follow";
+    followBtnColor.value = "success";
+  }
 }
 
 onMounted(() => {
@@ -68,7 +103,6 @@ onMounted(() => {
 const user = computed(() => store.state.user?.user);
 const posts = computed(() => store.state.user?.user?.posts);
 const authUser = computed(() => store.state.auth?.authUser);
-
 </script>
 <style scoped>
 .btn {
