5deab9195beaecd4838198a394bfb7b541433cbe
Reza <i.am.reza.programmer@gmail.com>
2024-02-13 21:18:31 +0330

    feat(edit.vue): search input

--- pages/users/[uuid]/edit.vue
+++ pages/users/[uuid]/edit.vue
@@ -3,34 +3,65 @@
     <v-row>
       <v-col lg="8" cols="12" class="mx-auto">
         <v-card class="px-5 py-10 d-flex flex-column align-center">
-          <div class="d-flex justify-center img-box rounded-circle overflow-hidden mx-auto mb-5">
+          <div
+            class="d-flex justify-center img-box rounded-circle overflow-hidden mx-auto mb-5"
+          >
             <img v-if="authUser?.avatar" :src="authUser?.avatar" />
             <img v-else src="@/assets/images/avatar.png" />
             <div class="camera-icon-box d-flex align-center justify-center">
-              <div class="d-flex flex-column justify-center align-center" v-if="!authUser?.avatar"
-                @click="fileInput.click()">
+              <div
+                class="d-flex flex-column justify-center align-center"
+                v-if="!authUser?.avatar"
+                @click="fileInput.click()"
+              >
                 <v-icon color="blue" size="50px">mdi-camera-outline</v-icon>
                 <p class="text">Add Avatar</p>
               </div>
-              <div class="d-flex flex-column justify-center align-center" v-else @click="removeAvatar()">
+              <div
+                class="d-flex flex-column justify-center align-center"
+                v-else
+                @click="removeAvatar()"
+              >
                 <v-icon color="red" size="50px">mdi-delete-outline</v-icon>
                 <p class="text">Delete Avatar</p>
               </div>
             </div>
-            <VFileInput accept="image/*" type="file" class="d-none" ref="fileInput" @change="uploadAvatar()" />
+            <VFileInput
+              accept="image/*"
+              type="file"
+              class="d-none"
+              ref="fileInput"
+              @change="uploadAvatar()"
+            />
           </div>
           <form ref="form" @submit.prevent="submitForm()">
             <div class="my-4">
-              <v-text-field :rules="firstNameRules" v-model="firstName" label="first name" variant="outlined"
-                ref="firstNameInput"></v-text-field>
+              <v-text-field
+                :rules="firstNameRules"
+                v-model="firstName"
+                label="first name"
+                variant="outlined"
+                ref="firstNameInput"
+              ></v-text-field>
             </div>
             <div class="my-4">
-              <v-text-field :rules="lastNameRules" v-model="lastName" label="last name" variant="outlined"
-                ref="lastNameInput"></v-text-field>
+              <v-text-field
+                :rules="lastNameRules"
+                v-model="lastName"
+                label="last name"
+                variant="outlined"
+                ref="lastNameInput"
+              ></v-text-field>
             </div>
-            <v-btn color="info" type="submit" class="py-6" block>
-              Edit
-            </v-btn>
+            <div class="my-4">
+              <input
+                type="text"
+                class="w-100 py-3 px-2 search-input"
+                ref="searchInput"
+                @input="searchHandler"
+              />
+            </div>
+            <v-btn color="info" type="submit" class="py-6" block> Edit </v-btn>
           </form>
         </v-card>
       </v-col>
@@ -41,14 +72,18 @@
 <script setup lang="ts">
 import { store } from "~/store";
 import { AuthActionTypes } from "~/store/auth/action-types";
-import { addUserAvatar, deleteUserAvatar, updateUserProfile } from "~/store/user/actions";
+import {
+  addUserAvatar,
+  deleteUserAvatar,
+  updateUserProfile,
+} from "~/store/user/actions";
 const router = useRouter();
 
 const authUser = computed(() => store.state.auth?.authUser);
 const route = useRoute();
 
 if (authUser.value?.uuid !== route.params.uuid) {
-  navigateTo('/404') 
+  navigateTo("/404");
 }
 
 const form = ref();
@@ -56,13 +91,25 @@ const firstNameInput = ref();
 const lastNameInput = ref();
 const fileInput = ref();
 
+const searchInput = ref();
+let timeout: any = null;
+
+function searchHandler() {
+  if (timeout) clearTimeout(timeout);
+
+  timeout = setTimeout(() => {
+    console.log("yes");
+  }, 1000);
+}
+
 const firstName: Ref<string> = ref("");
 const lastName: Ref<string> = ref("");
 
 const firstNameRules = ref([
   (value: string) => !!value.trim() || "This field is required",
   (value: string) =>
-    (value && value.length >= 3) || "first Name cannot have less than 3 letters",
+    (value && value.length >= 3) ||
+    "first Name cannot have less than 3 letters",
   (value: string) =>
     (value && value.length <= 20) ||
     "first Name cannot have more than 20 letters",
@@ -101,11 +148,14 @@ async function submitForm() {
     return;
   }
 
-  await updateUserProfile(authUser.value?.uuid, firstName.value, lastName.value);
+  await updateUserProfile(
+    authUser.value?.uuid,
+    firstName.value,
+    lastName.value
+  );
 
   router.push(`/`);
 }
-
 </script>
 
 <style scoped>
@@ -119,7 +169,7 @@ form {
   width: max-content;
 }
 
-.img-box>img {
+.img-box > img {
   width: 250px;
 }
 
@@ -132,9 +182,15 @@ form {
   cursor: pointer;
 }
 
-
 .text {
   color: white;
   font-size: 1.2rem;
+}
+
+.search-input {
+  font-size: 1.2rem;
+  outline: none;
+  border: 1px solid;
+  border-radius: 5px;
 }
 </style>
