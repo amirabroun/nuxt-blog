1a2423fc7634ed4d3eedf94ce61634c4c7f68c2f
Amir Abroun <abroon234@gmail.com>
2024-03-17 17:21:42 +0330

    pref: after update profile, update state

--- pages/users/[uuid]/edit.vue
+++ pages/users/[uuid]/edit.vue
@@ -1,83 +1,79 @@
 <template>
-  <v-container class="mt-10">
-    <v-row>
-      <v-col lg="8" cols="12" class="mx-auto">
-        <v-card class="px-5 py-6 d-lg-flex text-center">
-          <v-avatar class="elevation-4 mb-4 mr-4" size="260" rounded="lg">
-            <img
-              class="w-100"
-              v-if="authUser?.avatar"
-              :src="authUser?.avatar"
-            />
-            <img class="w-100" v-else src="@/assets/images/avatar.png" />
-            <v-btn
-              variant="tonal"
-              position="absolute"
-              size="100"
-              color="info"
-              rounded="circle"
-              v-if="!authUser?.avatar"
-              @click="fileInput.click()"
-            >
-              <VFileInput
-                accept="image/*"
-                type="file"
-                class="d-none"
-                ref="fileInput"
-                @change="uploadAvatar()"
-              />
-              <v-icon color="info" size="50px">mdi-camera-outline</v-icon>
-            </v-btn>
-            <v-btn
-              v-else
-              variant="tonal"
-              position="absolute"
-              size="100"
-              color="danger"
-              rounded="circle"
-              @click="removeAvatar()"
-            >
-              <v-icon color="danger" icon="mdi-delete" size="50px" />
-            </v-btn>
-          </v-avatar>
-          <form class="mx-auto w-75">
-            <v-text-field
-              :rules="firstNameRules"
-              v-model="firstName"
-              label="first name"
-              variant="solo-filled"
-              ref="firstNameInput"
-            />
-            <v-text-field
-              :rules="lastNameRules"
-              v-model="lastName"
-              label="last name"
-              variant="solo-filled"
-              ref="lastNameInput"
-            />
-            <v-text-field
-              v-model="username"
-              :rules="userNameRules"
-              @input="searchHandler"
-              label="username"
-              variant="solo-filled"
-              ref="usernameInput"
-            />
-            <v-card-actions>
-              <v-btn
-                type="submit"
-                color="info"
-                @click="submitForm()"
-                variant="outlined"
-              >
-                Update
-              </v-btn>
-            </v-card-actions>
-          </form>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-row class="mt-16" justify="center">
+    <v-card class="px-4 py-3 d-lg-flex text-center" width="650">
+      <v-avatar class="elevation-5 mb-4 mr-4" size="260" rounded="lg">
+        <img class="w-100" v-if="authUser?.avatar" :src="authUser?.avatar" />
+        <img class="w-100" v-else src="@/assets/images/avatar.png" />
+        <v-btn
+          variant="tonal"
+          position="absolute"
+          size="100"
+          color="info"
+          rounded="circle"
+          v-if="!authUser?.avatar"
+          @click="file.click()"
+        >
+          <v-file-input
+            accept="image/*"
+            type="file"
+            class="d-none"
+            ref="file"
+            @change="uploadAvatar()"
+          />
+          <v-icon color="info" size="50px">mdi-camera-outline</v-icon>
+        </v-btn>
+        <v-btn
+          v-else
+          variant="tonal"
+          position="absolute"
+          size="100"
+          color="danger"
+          rounded="circle"
+          @click="removeAvatar()"
+        >
+          <v-icon color="danger" icon="mdi-delete" size="50px" />
+        </v-btn>
+      </v-avatar>
+
+      <v-row>
+        <v-col cols="12" style="height: 60px">
+          <v-text-field
+            :rules="stringRules"
+            v-model="firstName"
+            label="first name"
+            variant="solo-filled"
+          />
+        </v-col>
+        <v-col cols="12" style="height: 60px">
+          <v-text-field
+            :rules="stringRules"
+            v-model="lastName"
+            label="last name"
+            variant="solo-filled"
+          />
+        </v-col>
+        <v-col cols="12" style="height: 60px">
+          <v-text-field
+            v-model="username"
+            :rules="stringRules"
+            @input="searchHandler"
+            label="username"
+            variant="solo-filled"
+          />
+        </v-col>
+        <v-card-actions class="ml-auto" style="height: 60px">
+          <v-btn
+            type="submit"
+            color="primary"
+            @click="submitForm()"
+            variant="tonal"
+          >
+            Update
+          </v-btn>
+        </v-card-actions>
+      </v-row>
+    </v-card>
+  </v-row>
 </template>
 
 <script setup lang="ts">
@@ -91,20 +87,14 @@ import {
 } from "~/store/user/actions";
 
 const authUser = computed(() => store.state.auth?.authUser);
-const route = useRoute();
-
-const firstNameInput = ref();
-const lastNameInput = ref();
-const fileInput = ref();
-const usernameInput: Ref<string> = ref("");
 
 const firstName = ref(<string>authUser.value?.first_name);
 const lastName = ref(<string>authUser.value?.last_name);
 const username = ref(<string>authUser.value?.username);
-
-let timeout: any = null;
+const file = ref();
 
 function searchHandler() {
+  let timeout: any = null;
   if (username.value == "") return;
   if (timeout) clearTimeout(timeout);
 
@@ -113,37 +103,17 @@ function searchHandler() {
   }, 1500);
 }
 
-const firstNameRules = ref([
-  (value: string) => !!value.trim() || "This field is required",
-  (value: string) =>
-    (value && value.length >= 3) ||
-    "first Name cannot have less than 3 letters",
-  (value: string) =>
-    (value && value.length <= 20) ||
-    "first Name cannot have more than 20 letters",
-]);
-
-const lastNameRules = ref([
-  (value: string) => !!value.trim() || "This field is required",
-  (value: string) =>
-    (value && value.length >= 3) || "User Name cannot have less than 3 letters",
-  (value: string) =>
-    (value && value.length <= 15) ||
-    "User Name cannot have more than 15 letters",
-]);
-
-const userNameRules = ref([
+const stringRules = ref([
   (value: string) => !!value.trim() || "Required",
   (value: string) =>
-    (value && value.length >= 3) || "User Name cannot have less than 3 letters",
+    (value && value.length >= 3) || "Cannot have less than 3 letters",
   (value: string) =>
-    (value && value.length <= 15) ||
-    "User Name cannot have more than 15 letters",
+    (value && value.length <= 15) || "Cannot have more than 15 letters",
 ]);
 
 async function uploadAvatar() {
   let formData = new FormData();
-  formData.append("avatar", fileInput.value.files[0]);
+  formData.append("avatar", file.value.files[0]);
 
   await addUserAvatar(authUser.value?.uuid, formData);
 
@@ -151,20 +121,12 @@ async function uploadAvatar() {
 }
 
 async function removeAvatar() {
-  if (authUser.value?.uuid !== route.params.uuid) {
-    return;
-  }
-
   await deleteUserAvatar(authUser.value?.uuid);
 
   await store.dispatch(`auth/${AuthActionTypes.fetchAuthUser}`);
 }
 
 async function submitForm() {
-  if (authUser.value?.uuid !== route.params.uuid) {
-    return;
-  }
-
   await updateUserProfile(
     authUser.value?.uuid,
     firstName.value,
@@ -172,9 +134,6 @@ async function submitForm() {
     username.value
   );
 
-  reloadNuxtApp({
-    path: `/users/${authUser.value.uuid}`,
-    force: true,
-  });
+  await store.dispatch(`auth/${AuthActionTypes.fetchAuthUser}`);
 }
 </script>
