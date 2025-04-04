d5101a5d39ef169753b255895ae7a7ad2f276b71
Amir Abroun <abroon234@gmail.com>
2025-04-04 11:20:56 +0330

    feat: add tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default {
   modules: ["nuxt-swiper"],
-  css: ["@fortawesome/fontawesome-svg-core/styles.css"],
+  css: [
+    "@fortawesome/fontawesome-svg-core/styles.css",
+    '~/assets/css/main.css'
+  ],
   build: {
     transpile: ["vuetify"],
   },
